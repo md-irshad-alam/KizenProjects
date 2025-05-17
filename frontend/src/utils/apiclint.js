e939d3eec93c7cff611b54f6e157c5144c578050
irshad <almairsh0205@gmail.com>
2025-05-17 11:28:40 +0530

    update json file

--- frontend/src/utils/apiclint.js
+++ frontend/src/utils/apiclint.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiclient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://kizenprojects.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
